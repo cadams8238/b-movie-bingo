80ad23f76bebdc2a22633ab0343de1039b3893c6
Courtney Adams <cadams8238@gmail.com>
2018-08-14 16:23:37 -0700

    rendering basic bingo card with hard coded data

---
+++ src/components/bingoCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>B-Movie Bingo</h1>
+    <section>
+      <Square label="Beefcake"/>
+      <Square label="High fall"/>
+      <Square label="Foot chase"/>
+      <Square label="Laser"/>
+      <Square label="Loved one injured or killed"/>
+      <Square label="Oiled up"/>
+      <Square label="Impaled, stabbed, or shot with arrow"/>
+      <Square label="'We had a deal'"/>
+      <Square label="Climbing or sneaking"/>
+      <Square label="Car bomb"/>
+      <Square label="Chewed out by chief"/>
+      <Square label="Suspended from force"/>
+      <Square label="BLANK SQUARE"/>
+      <Square label="Lone wolf attitude"/>
+      <Square label="T'n'A"/>
+      <Square label="Pointless flip"/>
+      <Square label="Drug or arms smuggling plot"/>
+      <Square label="Dream sequence"/>
+      <Square label="Training montage"/>
+      <Square label="Villian in a white suit"/>
+      <Square label="Eating shit"/>
+      <Square label="Drive through mall or building"/>
+      <Square label="Long boring scene"/>
+      <Square label="Awesome silhouette"/>
+      <Square label="Wasting food"/>
+    </section>
+  </div>
+</template>
+
+
+<script>
+  import Square from './square';
+
+  export default {
+    components: {
+      Square
+    }
+  }
+</script>
+
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+  
+  section {
+    width: 750px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+</style>
---
+++ src/components/square.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>{{ label }}</button>
+</template>
+
+
+<script>
+  export default {
+    props: [
+      'label'
+    ]
+  }
+</script>
+
+
+<style>
+  button {
+    box-sizing: border-box;
+    display: flex;
+    width: 20%;
+    height: 150px;
+    padding: 10px;
+    font-size: 16px;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+</style>
